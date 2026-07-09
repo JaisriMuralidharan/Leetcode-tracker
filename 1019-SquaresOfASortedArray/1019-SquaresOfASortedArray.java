8ed3b46e56e5ee5a2326d96134c253a6daf9c6c3
JaisriMuralidharan <m.jaisri18052006@gmail.com>
2026-07-09 11:28:54 +0530

    Create 1019-SquaresOfASortedArray

---
+++ 1019-SquaresOfASortedArray/1019-SquaresOfASortedArray.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 11:28:49 AM
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int index = nums.length-1;
+        int start = 0;
+        int end = nums.length-1;
+        while(start <= end){
+            if(Math.abs(nums[start]) > Math.abs(nums[end])){
+                result[index] = nums[start] * nums[start];
+                start++;
+                index--;
+            } else {
+                result[index] = nums[end] * nums[end];
+                end--;
+                index--;
+            }
+        }
+        return result;
+    }
+}
