4cbe821aab90b4d0ceac42c94d5e36e1b20c5fa1
JaisriMuralidharan <m.jaisri18052006@gmail.com>
2026-07-09 11:28:08 +0530

    Create 1503-ReducingDishes

---
+++ 1503-ReducingDishes/1503-ReducingDishes.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 11:28:04 AM
+class Solution {
+    public int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
+        int n = satisfaction.length, ans = 0, preSum = 0, timeCoefficient = 0;
+        for (int i = n-1; i >= 0; i--) {
+            if (preSum + satisfaction[i] <= 0) {
+                break;
+            }
+            preSum += satisfaction[i];
+            timeCoefficient += preSum;
+            ans = Math.max(ans, timeCoefficient);
+        }
+        return ans;
+    }
+}
